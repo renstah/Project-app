80e6eca19894c737d2f263920ecfb3f1f6aeb858
Walter de Valk <walter@school.erasmusmc.nl>
2015-06-14 13:50:35 +0200

    fixed the version error.

--- app/src/main/java/nl/hro/projectapp/LoginActivity.java
+++ app/src/main/java/nl/hro/projectapp/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
@@ -28,6 +29,8 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void onConnected(Bundle bundle) {
+        Toast.makeText(this, "apen", Toast.LENGTH_LONG);
+
         if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
             Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
             String personName = currentPerson.getDisplayName();
