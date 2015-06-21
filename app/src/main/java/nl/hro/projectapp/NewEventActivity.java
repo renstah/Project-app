fd13e00955984f32e64dff0dd117e206fd72ef4d
walterdevalk <walterdevalk@gmail.com>
2015-06-21 19:33:50 +0200

    most recent commit

--- app/src/main/java/nl/hro/projectapp/NewEventActivity.java
+++ app/src/main/java/nl/hro/projectapp/NewEventActivity.java
@@ -1,9 +1,14 @@
 package nl.hro.projectapp;
 
 import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -15,7 +20,7 @@ import nl.hro.projectapp.common.EventManager;
 public class NewEventActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event);
 
@@ -39,9 +44,27 @@ public class NewEventActivity extends ActionBarActivity {
         event.Longitude = 4.358891;
         event.Age_Min = 12;
 
+
         // event manager
         EventManager evmgr = new EventManager(getApplicationContext());
 
         // evmgr.CreateEvent(event); // verstuur naar db?
     }
+
+    public void submitNewEvent(){
+        // get project name
+        String event_name = (String) findViewById(R.id.input_event_name).toString();
+
+        // get time fields
+        TimePicker start_time = (TimePicker) findViewById(R.id.input_start_time);
+        TimePicker end_time = (TimePicker) findViewById(R.id.input_end_time);
+        DatePicker start_date = (DatePicker) findViewById(R.id.input_start_date);
+        // TODO end date
+
+        // get
+
+
+    }
+
+
 }
